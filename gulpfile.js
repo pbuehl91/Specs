828f951748bd42a6b0bf23e785fd04b73af64daf
Patrick B. Buehl <pbb91@yahoo.com>
2016-12-24 20:27:23 -0600

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,31 @@
 var gulp = require('gulp'),
     bump = require('gulp-bump'),
     concat = require('gulp-concat'),
-    connect = require('gulp-connect',)
+    connect = require('gulp-connect'),
     debug = require('gulp-debug'),
     gzip = require('gulp-gzip'),
     minifyCss = require('gulp-minify-css'),
-    protractor = require('gulp-protractor'),
     sass = require('gulp-sass'),
     tar = require('gulp-tar'),
     tsc = require('gulp-typescript'),
     tslint = require('gulp-tslint'),
     typings = require('typings'),
     template = require('gulp-template'),
+    templateCache = require('gulp-angular-templatecache'),
     tsd = require('tsd'),
-    del = require('del'),
-    browserSync = require('browser-sync');
+    del = require('del');
+
+/*
+ *  Automated gulp tasks
+ */
+gulp.task('dev', ['start','watch']);
+gulp.task('start', start); //build app and start server
+gulp.task('watch', watch); //will watch for changes in files and redeploy
+
+function start() {
+    console.log('hecarim');
+}
+
+function watch() {
+    console.log('passive');
+}
